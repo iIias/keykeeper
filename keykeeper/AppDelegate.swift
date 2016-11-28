8627045f54b50c66a7e8294f08275d05d640b341
Ilias May <ilias.may@icloud.com>
2016-11-28 17:16:31 +0100

    Moved copy & delete submenu in one loop

--- keykeeper/AppDelegate.swift
+++ keykeeper/AppDelegate.swift
@@ -5,6 +5,24 @@
 //  Created by Ilias Ennmouri on 07/08/16.
 //  Copyright © 2016 keykeeper. All rights reserved.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 
 import Cocoa
 import KeychainAccess
@@ -17,7 +35,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
     let delegate = AppDelegate.self
     var appKeychain = Keychain(service: "co.keykeeper.keykeeper")
-    var internetKeychain = Keychain(server: "https://example.com", protocolType: .https, authenticationType: .htmlForm)
     let pasteboard = NSPasteboard.general()
     var eventMonitor: EventMonitor?
     let menu = NSMenu()
@@ -52,10 +69,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         deletePasswordItem.submenu = deletePasswordMenu
         addKeyItem.submenu = addKeyMenu
         
-        for (index, value) in internetKeychain.allKeys().enumerated() {
+      /*  for (index, value) in internetKeychain.allKeys().enumerated() {
             print("Item \(index): \(value)")
             deletePasswordMenu.addItem(NSMenuItem(title: "🚮 \(value)", action: #selector(AppDelegate.deleteKey), keyEquivalent: ""));
-            clipboardMenu.addItem(NSMenuItem(title: "🔗 \(value)", action: #selector(AppDelegate.copyToPasteboard), keyEquivalent: "")); } }
+            clipboardMenu.addItem(NSMenuItem(title: "🔗 \(value)", action: #selector(AppDelegate.copyToPasteboard), keyEquivalent: "")); } */ } 
     
     func refreshMenu() {
         menu.removeAllItems()
@@ -108,12 +125,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func copyToPasteboard() {
         pasteboard.clearContents()
-        let token = try! internetKeychain.get("\(copyString)")
+        let token = try! addInternetPasswordVC().internetKeychain.get("\(copyString)")
         pasteboard.setString("\(token!)", forType: NSPasteboardTypeString) }
     
     func deleteKey() {
         do {
-        try internetKeychain.remove("\(deleteString)")
+        try addInternetPasswordVC().internetKeychain.remove("\(deleteString)")
         print("key: \(deleteString) has been removed")
     } catch let error {
         print("error: \(error)") }
